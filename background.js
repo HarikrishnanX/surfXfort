96beea05d6d7018b90bae982bf30601b506b2023
Harikrishnan V <44549721+HarikrishnanX@users.noreply.github.com>
2021-06-07 23:07:14 +0530

    Add files via upload

---
+++ background.js
@@ -0,0 +1,2 @@
+chrome.browserAction.onClicked.addListener(function(a){chrome.windows.getCurrent(function(a){parentWindowId=a.id})
+window.open(chrome.extension.getURL("popup.html?tabid="+encodeURIComponent(a.id)+"&url="+a.url),"Table Scraper","toolbar=0,scrollbars=0,location=0,statusbar=0,menubar=0,resizable=1,width=720,height=650")});
